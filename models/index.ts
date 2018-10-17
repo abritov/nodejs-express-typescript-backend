09fb8995b1bebbd4d568f52a5122709b0f945b65
Alexander Britov <pkgsail.dev@gmail.com>
2018-10-17 13:57:09 +0300

    feat(model): add index.ts

--- models/index.ts
+++ models/index.ts
@@ -1,19 +1,17 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
+var fs        = require('fs');
+var path      = require('path');
 var Sequelize = require('sequelize');
-var basename = path.basename(__filename);
-var env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/../config/config.json')[env];
-var db: any = {};
-
-let sequelize;
+var basename  = path.basename(__filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db        = {};
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
@@ -35,4 +33,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-export default db;
+module.exports = db;
