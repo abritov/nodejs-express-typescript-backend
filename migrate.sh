332a2c653348d3d9054ad9f1015393ba426b3d7a
Alexander Britov <pkgsail.dev@gmail.com>
2018-10-17 15:17:07 +0300

    feat(project): init migrate script

---
+++ migrate.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Read arguments passed to the script.
+if [ -z "$1" ]; then
+ ENVIRONMENT='development'
+else
+ ENVIRONMENT="$1"
+fi
+
+echo ""
+echo "Migrating for environment: $ENVIRONMENT"
+echo ""
+
+echo " -> Step 1/4: Compiling migration scripts."
+echo ""
+for filename in ./src/db/migrations/*.ts; do
+ yarn tsc --target ES6 --module CommonJS --noEmitOnError --outDir ./build-migrations $filename
+done
+echo ""
+echo " -> Compilation completed."
+echo ""
+
+# echo ""
+# echo " -> Step 2/4: Copying resources required for migration."
+# cp -rf ./src/db/dump ./build-migrations/db/
+# echo " -> Copying resources completed."
+# echo ""
+
+# echo ""
+# echo " -> Step 3/4: Starting migration."
+# echo ""
+# yarn sequelize db:migrate - env $ENVIRONMENT
+# echo ""
+# echo " -> Migration completed."
+# echo ""
+
+# echo ""
+# echo " -> Step 4/4: Deleting generated files."
+# echo ""
+# rm -Rf ./build-migrations
+# mkdir ./build-migrations
+# echo ""
+# echo " -> Deletion completed."
