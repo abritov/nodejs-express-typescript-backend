25ecce7ab123fb2cc09feedbd17371b8e974ba0a
Alexander Britov <pkgsail.dev@gmail.com>
2018-10-17 17:28:00 +0300

    fix(migrations): remove samples

--- src/db/migrations/promise.ts
+++
@@ -1,9 +0,0 @@
-import { QueryInterface, SequelizeStatic } from 'sequelize';
-
-export function up(queryInterface: QueryInterface, Sequelize: SequelizeStatic) {
-  return queryInterface.createTable('Users', {}, {});
-};
-
-export function down(queryInterface: QueryInterface, Sequelize: SequelizeStatic) {
-  return queryInterface.dropTable('Users');
-};
