6b63894feb856dd78eb98b6f4137b48d60df32cd
Alexander Britov <pkgsail.dev@gmail.com>
2018-10-19 13:56:44 +0300

    fix(migrations): create_users_table add fields

--- src/db/migrations/20181017172812-create_users_table.ts
+++ src/db/migrations/20181017172812-create_users_table.ts
@@ -15,7 +15,7 @@ export function up(queryInterface: QueryInterface, Sequelize: SequelizeStatic) {
       allowNull: false,
       validate: {
         notEmpty: true,
-      }
+      },
     },
     authKey: {
       field: 'auth_key',
@@ -26,6 +26,22 @@ export function up(queryInterface: QueryInterface, Sequelize: SequelizeStatic) {
       field: 'password_hash',
       type: Sequelize.STRING(255),
       allowNull: true,
+    },
+    salt: {
+      field: 'salt',
+      type: Sequelize.STRING(255),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    oldHash: {
+      field: 'old_hash',
+      type: Sequelize.STRING(255),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
     passwordResetToken: {
       field: 'password_reset_token',
@@ -40,18 +56,31 @@ export function up(queryInterface: QueryInterface, Sequelize: SequelizeStatic) {
       validate: {
         notEmpty: true,
         isEmail: true,
-      }
+      },
     },
-    createdAt: {
-      field: 'created_at',
+    statusId: {
+      field: 'status_id',
       type: Sequelize.INTEGER(10),
       allowNull: false,
+      defaultValue: 0,
     },
-    updatedAt: {
-      field: 'updated_at',
-      type: Sequelize.INTEGER(10),
+    approved: {
+      field: 'approved',
+      type: Sequelize.BOOLEAN,
       allowNull: false,
+      defaultValue: false,
     },
+    lastLogin: {
+      field: 'last_login',
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+    createtAt: {
+      field: 'createt_at',
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW
+    }
   })
 };
 
