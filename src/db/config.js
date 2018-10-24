3b90d9241a756917505bf30292ef86838ac73608
Alexander Britov <pkgsail.dev@gmail.com>
2018-10-24 13:01:15 +0300

    fix(db): change config.ts -> js

--- src/db/config.js
+++ src/db/config.js
@@ -1,26 +1,9 @@
-export default {
+const config = {
   development: {
     username: "root",
     password: "root",
     database: "ranobe_v2",
     host: "127.0.0.1",
-    dialect: "mysql",
-    sync: {
-      force: false
-    },
-    define: {
-      underscored: false,
-      freezeTableName: true,
-      charset: "utf8",
-      timestamps: false
-    }
-  },
-  development_int09h: {
-    username: "root",
-    password: "root",
-    database: "ranobe_v2",
-    host: "arch",
-    port: 3308,
     dialect: "mysql",
     sync: {
       force: false
@@ -65,3 +48,11 @@ export default {
     }
   }
 }
+
+config['development_int09h'] = {
+  ...config.development,
+  host: "arch",
+  port: 3308,
+}
+
+module.exports = config;
