ed9ec9a783745a43ab02043de32ddbe66bafb22c
Alexander Britov <pkgsail.dev@gmail.com>
2018-11-09 15:04:54 +0300

    feat(auth): add createJwtStrategy

--- src/controllers/v1/auth.controller.ts
+++ src/controllers/v1/auth.controller.ts
@@ -1,6 +1,30 @@
 import { Router, Request } from 'express';
 import { AuthSignUp, AuthorizationToken } from './schema';
+import { StrategyOptions, Strategy, ExtractJwt, VerifiedCallback } from 'passport-jwt';
 import { DbApi } from '../../db';
+
+interface JwtPayload {
+  userId: number,
+  roleId: number,
+  name: string
+}
+
+export function createJwtStrategy(db: DbApi, secret?: string) {
+
+  const jwtOptions: StrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: secret || 'SCugV4e4Z6DTZzXmfYbHqh9KlblOSHVL8tpqy0gO3+W7ylryT',
+    algorithms: ['HS256']
+  };
+  return new Strategy(jwtOptions, async (payload: JwtPayload, done: VerifiedCallback) => {
+    const user = await db.User.findById(payload.userId);
+    if (user) {
+      return done(null, user);
+    }
+    return done('cannot find user', null);
+  });
+
+}
 
 export class AuthController {
   db: DbApi;
