86ff5509a58b71a3b5f41480df0c3a7656c4e9f3
Alexander Britov <pkgsail.dev@gmail.com>
2018-11-10 14:44:33 +0300

    feat(utils): add Hasher

---
+++ src/utils/hasher.ts
@@ -0,0 +1,22 @@
+export interface Hasher {
+  createHash(data: string, salt?: string): string
+  validate(data: string, hash: string, salt?: string): boolean
+}
+
+export class MockHasher implements Hasher {
+  createHash(data: string, salt?: string | undefined): string {
+    return data + "." + salt || "mock_salt";
+  }
+  validate(data: string, hash: string, salt?: string | undefined): boolean {
+    return (data + "." + salt || "mock_salt") == hash;
+  }
+}
+
+export class BcryptHasher implements Hasher {
+  createHash(data: string, salt?: string | undefined): string {
+    throw new Error("Method not implemented.");
+  }
+  validate(data: string, hash: string, salt?: string | undefined): boolean {
+    throw new Error("Method not implemented.");
+  }
+}
