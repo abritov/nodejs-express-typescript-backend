713161f5fce04db867c276c00a5cf5158f02408c
Alexander Britov <pkgsail.dev@gmail.com>
2018-11-11 23:11:28 +0300

    fix(token): create implement

--- src/controllers/v1/token.controller.ts
+++ src/controllers/v1/token.controller.ts
@@ -4,14 +4,18 @@ import passport = require('passport');
 import { DbApi } from '../../db';
 import { Hasher } from '../../utils/hasher';
 import { User } from '../../db/models/User';
-import { Jwt } from './authenticate';
+import { Jwt, JwtPayload } from './authenticate';
 
 export class TokenController {
   constructor(public _db: DbApi, public _hasher: Hasher, public _jwt: Jwt) { }
 
-  async create(request: CreateToken) {
-    const user = await this._db.User.findOne({ where: { email: request.email } });
-    if (user) return <AuthorizationToken>{ token: '123' }
+  async create(request: CreateToken, user: User) {
+    const token = this._jwt.encrypt(<JwtPayload>{
+      userId: user.id!,
+      name: user.name,
+      accessBitmask: request.accessBitmask
+    });
+    return <AuthorizationToken>{ token }
   }
 }
 
@@ -19,17 +23,22 @@ export function createTokenRouter(db: DbApi, hasher: Hasher, jwt: Jwt) {
   const router = Router();
   const controller = new TokenController(db, hasher, jwt);
 
-  router.post('/insecure', (req: Request, res: Response) => {
+  router.post('/insecure', async (req: Request, res: Response) => {
     if (req.connection.localAddress != "127.0.0.1") {
       res.status(403).send();
       return;
     }
-    res.json(controller.create(<CreateToken>req.body));
+    const user = await db.User.findOne({ where: { email: req.body.email } });
+    if (!user) {
+      res.status(404).json({ error: "user not found" });
+      return;
+    }
+    res.json(controller.create(<CreateToken>req.body, user));
   });
 
   router.post('/', passport.authenticate('local', { session: false }), async (req: Request, res: Response) => {
     const user: User = req.user;
-    res.json(await controller.create(<CreateToken>req.body));
+    res.json(await controller.create(<CreateToken>req.body, req.user));
   });
 
   return router;
