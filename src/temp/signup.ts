1d49fdd15985c94c6dfb40ac4861b3e1124d08a4
Alexander Britov <pkgsail.dev@gmail.com>
2018-11-15 10:05:53 +0300

    feat(project): add signup temp

---
+++ src/temp/signup.ts
@@ -0,0 +1,26 @@
+export interface SignupTempRecord {
+  name: string
+  profile: any
+}
+
+export interface SignupTemp {
+  get(accessToken: string): SignupTempRecord | undefined
+  set(accessToken: string, value: SignupTempRecord): void
+  has(accessToken: string): boolean
+}
+
+export class SignupTempMemory implements SignupTemp {
+  store: Map<string, SignupTempRecord> = new Map<string, SignupTempRecord>();
+
+  has(accessToken: string): boolean {
+    return this.store.has(accessToken);
+  }
+
+  get(accessToken: string): SignupTempRecord | undefined {
+    return this.store.get(accessToken);
+  }
+
+  set(accessToken: string, value: SignupTempRecord): void {
+    this.store.set(accessToken, value);
+  }
+}
