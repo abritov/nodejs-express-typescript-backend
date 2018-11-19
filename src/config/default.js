cd2a416de66a02864b5c66b07a64fdb67732c645
Alexander Britov <pkgsail.dev@gmail.com>
2018-11-19 17:12:39 +0300

    feat(config): add type annotations

--- src/config/default.js
+++ src/config/default.js
@@ -1,11 +1,16 @@
+// @ts-check
+
+/**
+ * @param {import('./types').Config} config
+ */
 const config = {
   db: {
     username: "",
     password: "",
     database: "ranobe_v2",
     host: "127.0.0.1",
-		dialect: "mysql",
-		port: 3306,
+    dialect: "mysql",
+    port: 3306,
     operatorsAliases: false,
     sync: {
       force: false
@@ -16,8 +21,8 @@ const config = {
       charset: "utf8",
       timestamps: false
     }
-	},
-	jwtSecret: 'SCugV4e4Z6DTZzXmfYbHqh9KlblOSHVL8tpqy0gO3+W7ylryT',
+  },
+  jwtSecret: 'SCugV4e4Z6DTZzXmfYbHqh9KlblOSHVL8tpqy0gO3+W7ylryT',
 }
 
 module.exports = config;
