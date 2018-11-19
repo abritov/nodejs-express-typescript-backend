a43ff179756d68cbd706dcf53646a6312a1b0d43
Alexander Britov <pkgsail.dev@gmail.com>
2018-11-19 17:18:44 +0300

    refactor(db): config

--- src/db/config.js
+++ src/db/config.js
@@ -1,23 +1,4 @@
-const defaultConfig = require('./../config/default.js');
-
-if (!process.env.NODE_ENV) {
-	process.env.NODE_ENV = 'development';
-}
-
-let config = {};
-
-switch (process.env.NODE_ENV) {
- case 'development':
-	 config = require('./../config/dev.js');
-	 config = {...defaultConfig, ...config};
-	 break;
-
-	 case 'production':
-		config = require('./../config/prod.js');
-		config = {...defaultConfig, ...config};
-		break;
-}
-
 module.exports = {
-	[process.env.NODE_ENV] : config.db
-};
+  development: require('../config/dev').db,
+  production: require('../config/prod').db
+}
