18e36ebfdb3ad4527abc54bd6178db5943c1cdf7
Alexander Britov <pkgsail.dev@gmail.com>
2018-12-11 12:14:39 +0300

    feat(controllers): add ChapterController

---
+++ src/controllers/v1/chapter.controller.ts
@@ -0,0 +1,21 @@
+import { DbApi } from '../../db/index';
+
+export class ChapterController {
+  constructor(public _db: DbApi) { }
+
+  get() {
+
+  }
+
+  create() {
+
+  }
+
+  update() {
+
+  }
+
+  progress() {
+
+  }
+}
