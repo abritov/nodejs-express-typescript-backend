9d73e4712c1c87bbfc0ce84f16a3ef59cdaff18a
Alexander Britov <pkgsail.dev@gmail.com>
2018-12-11 12:14:31 +0300

    feat(controllers): add BookController

---
+++ src/controllers/v1/book.controller.ts
@@ -0,0 +1,29 @@
+import { DbApi } from '../../db/index';
+
+export class BookController {
+  constructor(public _db: DbApi) { }
+
+  get() {
+
+  }
+
+  create() {
+
+  }
+
+  update() {
+
+  }
+
+  like() {
+
+  }
+
+  dislike() {
+
+  }
+
+  rate() {
+
+  }
+}
