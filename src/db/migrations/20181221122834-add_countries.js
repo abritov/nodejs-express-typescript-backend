655915b22b47daed58cd369eac152728d4590adf
Alexander Britov <pkgsail.dev@gmail.com>
2018-12-21 12:32:36 +0300

    fix(db): add countries

---
+++ src/db/migrations/20181221122834-add_countries.js
@@ -0,0 +1,48 @@
+// @ts-check
+
+/**
+ * @param {import('sequelize').QueryInterface} queryInterface
+ * @param {import('sequelize').SequelizeStatic} Sequelize
+ */
+module.exports.up = (queryInterface, Sequelize) => {
+  return queryInterface.bulkInsert('country', [
+    {
+      name: 'Великобритания',
+      alias: 'England'
+    },
+    {
+      name: 'Корея',
+      alias: 'Korea'
+    },
+    {
+      name: 'Китай',
+      alias: 'China'
+    },
+    {
+      name: 'Япония',
+      alias: 'Japan'
+    },
+    {
+      name: 'Россия',
+      alias: 'Russia'
+    },
+    {
+      name: 'США',
+      alias: 'USA'
+    },
+  ]);
+};
+
+/**
+ * @param {import('sequelize').QueryInterface} queryInterface
+ * @param {import('sequelize').SequelizeStatic} Sequelize
+ */
+module.exports.down = (queryInterface, Sequelize) => {
+  return queryInterface.bulkDelete('country', {
+    where: {
+      id: {
+        [Sequelize.Op.gte]: 1
+      }
+    }
+  });
+};
